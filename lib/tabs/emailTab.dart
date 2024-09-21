1840534da7532ffd7027a57ec89f06b6a7ba7154
WashiArnob <abuwashi.arnob7275924@gmail.com>
2024-09-21 09:37:09 +0600

    3rd commit

---
+++ lib/tabs/emailTab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class EmailTab extends StatelessWidget {
+  const EmailTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Email Page'),
+    );
+  }
+}
---
+++ lib/tabs/settingTab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class SettingTab extends StatelessWidget {
+  const SettingTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Setting Page'),
+    );
+  }
+}
